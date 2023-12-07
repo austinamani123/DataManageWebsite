71bb2eb1321aa5942a4dfb9d1b87ef3551444287
Austin Amani <amaniaustin123@gmail.com>
2023-12-07 10:09:51 +0300

    Created the Navbar using Tailwind and React

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useState } from 'react'
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false)
+  
+  const handleNav = () => {
+    setNav(!nav)
+  }
+
+  return (
+    <nav className="nav">
+        <h1>React</h1>
+        <ul className="hidden md:flex">
+            <li className="p-4">Home</li>
+            <li className="p-4">Company</li>
+            <li className="p-4">Resources</li>
+            <li className="p-4">About</li>
+            <li className="p-4">Contact</li>
+        </ul>
+        <div onClick={handleNav} className='block md:hidden'>
+            {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
+            
+        </div>
+        <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' 
+          : 'fixed left-[-100%] top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'}>
+            <h1 className="w-full m-4 text-3xl py-4 font-bold text-[#00df9a]">React</h1>
+                <ul className="p-2">
+                    <li className="p-4 border-b border-gray-600">Home</li>
+                    <li className="p-4 border-b border-gray-600">Company</li>
+                    <li className="p-4 border-b border-gray-600">Resources</li>
+                    <li className="p-4 border-b border-gray-600">About</li>
+                    <li className="p-4">Contact</li> 
+                </ul>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
